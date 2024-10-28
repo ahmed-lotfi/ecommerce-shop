c24ab12fa3048eb92f16b21b7bda9348dca2b446
Ahmed Mohamed Lotfi <moahmedm559@gmail.com>
2024-10-28 17:49:39 +0300

    uploading files to github

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div className="overflow-hidden">
+      <BrowserRouter>
+        <Header />
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
